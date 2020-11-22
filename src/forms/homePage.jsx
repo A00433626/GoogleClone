af17c1c722f2470a3bd8fce86d863c6f1427f048
vtejakatragadda@perennia.ca+1-at-290587490288 <vtejakatragadda@perennia.ca>
2020-11-22 16:09:06 -0400

    Styled Nav bar

--- src/forms/homePage.jsx
+++ src/forms/homePage.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import './homePage.css';
+import './HomePage.css';
 import { Link } from 'react-router-dom';
-
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';
 // API
 // AIzaSyCGtt8IzUCfqsGYsP2fQ1P6rEt5kyqBqqI
 function HomePage() {
 	return <div className='header'>
-		<div>Hi</div>
 		<div className="homepage_nav-header">
 			<div className="homepage_nav-header-left">
 				{/* About link */}
+				<Link to='./about'>About</Link>
 				{/* store link */}
+				<Link to='./store'>Store</Link>
 			</div>
 			<div className="homepage_nav-header-right">
 				{/* gmail link */}
+				<Link to='./gmail'>Gmail</Link>
 				{/* images link */}
-				{/* 9 dots icon link */}
+				<Link to='./images'>Images</Link>
+				{/* Apps icon link */}
+				<AppsIcon/>
 				{/* user icon link */}
+				<Avatar/>
 			</div>
 		</div>
 		<div className="homepage_nav-body"></div>
