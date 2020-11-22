b187fd1e71078cef1dc946be0a453c44bad29ef2
vtejakatragadda@perennia.ca+1-at-290587490288 <vtejakatragadda@perennia.ca>
2020-11-22 19:10:29 -0400

    Added the search bar function

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Search.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import {Button} from '@material-ui/core'
+import {useHistory} from 'react-router-dom';
 function Search() {
+    const [inputData,setInputData]=useState('')
+    const history=useHistory();
+    const searchClicked=(event)=>{
+      event.preventDefault();
+      if (event.key === 'Enter' || event.keyCode === 13) {
+        history.push('/search')
+    }
+      history.push('/search')
+    }
     return (
     <div className="search-box">
 <div className="search-box-input">
     {/* Search Icon */}
     <SearchIcon className='search-box-input-search-icon'/>
     {/* Input field */}
-    <input/>
+    <input value={inputData} onChange={(e)=>{setInputData(e.target.value)}}/>
       {/* Mic Icon */}
     <MicIcon className='search-box-input-microphone-icon'/>
 </div>
 <div className="search-box-buttons">
-    <Button variant='outlined'>Google Search</Button>
+    <Button type='submit' variant='outlined' onClick={searchClicked}>Google Search</Button>
     <Button variant='outlined'>I'm Feeling Lucky</Button>
 </div>
 </div>
